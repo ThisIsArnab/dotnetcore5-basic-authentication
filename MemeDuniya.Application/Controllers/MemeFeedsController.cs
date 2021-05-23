f249f6138685a70ae780ed6c1705afb4f224e35f
Arnab Das <das92.arnab@gmail.com>
2021-05-23 10:10:36 +0530

    Refactored controller file name

---
+++ MemeDuniya.Application/Controllers/MemeFeedsController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MemeDuniya.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MemeDuniya.Application.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MemeFeedsController : ControllerBase
+    {        
+        private readonly ILogger<MemeFeedsController> _logger;
+
+        public MemeFeedsController(ILogger<MemeFeedsController> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Returns paginated list of Feeds. Default feed count is 10.
+        /// Use cursor to get next set of feeds.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IEnumerable<Feed> GetFeeds()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Adds a feed from user.
+        /// </summary>
+        /// <param name="feed"></param>
+        [HttpPost]
+        public IActionResult AddFeed(Feed feed) 
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Return at most 10 filtered feeds. Only indexed criteria allowed
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IEnumerable<Feed> FilterFeeds()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Inclue User feeds at top if authenticated.
+        /// </summary>
+        /// <param name="keywords"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public IEnumerable<Feed> SearchFeeds(string[] keywords)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
